e01a195985c13e4d7cfe590797dabf7bc90c6ebe
Matheus Mendes <matheusmendesc.16@gmail.com>
2022-09-24 16:18:05 -0300

    Created mockers for listed all registers

---
+++ src/main/java/com/example/demo/service/PersonServices.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Person;
+
+@Service
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	public Person findById(String id) {
+		logger.info("Finding one person!");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Matheus");
+		person.setLastName("Mendes");
+		person.setAddress("Osasco - São Paulo - Brasil");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil" + i);
+		person.setGender("Male");
+		return person;
+	}
+}
